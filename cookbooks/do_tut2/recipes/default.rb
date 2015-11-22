29f99c8342006a3aadf53751d0e4905efb52191d
Brian Flannery <brian.fannery@gmail.com>
2015-11-22 04:38:42 +0000

    Works at last

--- cookbooks/do_tut2/recipes/default.rb
+++ cookbooks/do_tut2/recipes/default.rb
@@ -4,11 +4,14 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-file 'motd' do
-  content 'hello chef'
+node.default['thisUser'] = ENV['USER']
+node.default['dest'] = ENV['HOME']
+
+file "#{node['dest']}/motd" do
+  content "hello #{node['thisUser']}"
 end
 
-template 'chef-repo/cookbooks/hello_chef_server/recipes/default.rb' do
+template "#{node['dest']}/chef-repo/cookbooks/hello_chef_server/recipes/default.rb" do
   source 'hello_chef_server.default.rb.erb'
 end
 
