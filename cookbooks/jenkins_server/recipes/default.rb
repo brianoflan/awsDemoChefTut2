a63098a2c7191b8e5709cc3f49d729c1ac0b1f99
Brian Flannery <brian.flannery@gmail.com>
2015-11-23 21:30:50 +0000

    Some progress.

--- cookbooks/jenkins_server/recipes/default.rb
+++ cookbooks/jenkins_server/recipes/default.rb
@@ -4,11 +4,19 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-node.default['homeDest'] = "/home/#{ENV['SUDO_USER']}"
+# node.default['homeDest'] = "/home/#{ENV['SUDO_USER']}"
+#     theUser = ENV['SUDO_USER'] || ENV['USER']
+#     node.default['homeDest'] = "/home/#{theUser}" 
+
+randFile = '/tmp/jenkins_server.rand'
+
+node.default['jenkins-server']['security']['chef-vault']['data_bag'] = 'jenkins_server'
+node.default['jenkins-server']['security']['chef-vault']['data_bag_item'] = 'admin'
+node.default['dev_mode'] = 'true'
 
 execute 'opensslRand' do
   action :nothing
-  command 'openssl rand -base64 512 | tr -d \'\r\n\' > /tmp/jenkins_server.rand'
+  command "openssl rand -base64 512 | tr -d '\r\n' > #{randFile}"
   not_if { File.exists?('/tmp/jenkins_server.rand') }
 end
 
@@ -26,21 +34,29 @@ end
 
 template 'jserver_dbg' do
   action :nothing
-  path "#{node['homeDest']}/chef-repo/data_bags/jenkins_server/admin.json"
+  path lazy{"#{node['homeDest']}/chef-repo/data_bags/jenkins_server/admin.json"}
   source 'jserver_dbg.json.erb'
 end
 
+ruby_block 'findHome' do
+  block do
+    theUser = ENV['SUDO_USER'] || ENV['USER']
+    node.default['homeDest'] = "/home/#{theUser}" 
+  end
+  # notifies :run, 'ruby_block[prepInputFiles1]', :immediately
+end
 ruby_block 'prepInputFiles1' do
   block do
   end
-  notifies :run, 'execute[opensslRand]', :immediately
+  # notifies :run, 'execute[opensslRand]', :immediately
   notifies :run, 'execute[opensslNewkey]', :immediately
   notifies :run, 'ruby_block[prepInputFiles2]', :delayed
+  action :nothing
 end
 ruby_block 'prepInputFiles2' do
   action :nothing
   block do
-    node.default['jenkins_server_privKey'] = File.open('/var/tmp/.chef-zero.pem').read
+    node.default['jenkins_server_privKey'] = File.open('/var/tmp/.chef-zero.pem').read.gsub(/[\n\r]/, "\\n")
   end
   notifies :run, 'execute[opensslPubout]', :immediately
   notifies :run, 'ruby_block[setProps]', :delayed
@@ -48,17 +64,33 @@ end
 ruby_block 'setProps' do
   action :nothing
   block do
-    node.default['jenkins_server_pubKey'] = File.open('/var/tmp/.chef-zero.pub').read
+    node.default['jenkins_server_pubKey'] = File.open('/var/tmp/.chef-zero.pub').read.gsub(/([\n][\r]|[\n]|[\r])/, "\\n")
+    
   end
   notifies :create, 'template[jserver_dbg]', :immediately
   notifies :run, 'execute[knifeMkDbg]', :delayed
 end
 execute 'knifeMkDbg' do
   command 'knife data bag create jenkins_server'
+  cwd lazy{"#{node['homeDest']}/chef-repo"}
+  action :nothing
+  notifies :run, 'execute[knifeUploadDbg]', :immediately
+end
+execute 'knifeUploadDbg' do
+  # command lazy{"knife data bag --config #{node['homeDest']}/chef-repo/.chef/knife.rb from file jenkins_server admin.json --secret-file #{randFile}"}
+  command lazy{"knife data bag --config #{node['homeDest']}/chef-repo/.chef/knife.rb from file jenkins_server admin.json"}
+  cwd lazy{"#{node['homeDest']}/chef-repo"}
+  action :nothing
+  notifies :run, 'ruby_block[runJenkinsServer]', :delayed
+end
+ruby_block 'runJenkinsServer' do
+  block do
+    # include_recipe 'jenkins-server::default'
+    # run_context.include_recipe 'jenkins-server::default'
+  end
   action :nothing
 end
 
-# include_recipe 'jenkins-server::default'
-
+include_recipe 'jenkins-server::default'
 #
 
