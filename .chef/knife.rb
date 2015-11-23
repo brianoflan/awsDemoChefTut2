49295319bb72d39126bf4cedcebfe3286f30860f
Brian Flannery <Brian.Flannery@gmail.com>
2015-11-23 21:39:40 +0000

    Better way to do ~ and relative paths in knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,5 +1,8 @@
+
+current_dir = File.dirname(__FILE__)
+cookbook_path     "#{current_dir}/../cookbooks"
+
 chef_server_url   'http://127.0.0.1:8889'
 node_name         'stickywicket'
 client_key        '/var/tmp/.chef-zero.pem'
-cookbook_path     '~/chef-repo/cookbooks'
 
